654d240ab244faf5cf0ea989103beace1a99d8b9
abhinav-reddy-11 <abhinavjakkam@gmail.com>
2026-06-17 16:47:39 +0530

    Fix React imports

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import  React,{ useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import Editor from "@monaco-editor/react";
 import { useNavigate } from "react-router-dom";
